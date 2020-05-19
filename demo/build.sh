3774e5e519552268cae4a63d7797c987cd3f3f4c
George Hotz <geohot@gmail.com>
2020-05-19 16:33:16 -0700

    fix tabs

--- demo/build.sh
+++ demo/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
-PATH=/raid/Qualcomm/Hexagon_SDK/3.5.1/tools/HEXAGON_Tools/8.3.07/Tools/bin:$PATH
-PATH=/raid/Qualcomm/Hexagon_SDK/3.5.1/tools/qaic/Ubuntu16:$PATH
-G0=/raid/Qualcomm/Hexagon_SDK/3.5.1/tools/HEXAGON_Tools/8.3.07/Tools/target/hexagon/lib/v60/G0
+SDK=/raid/Qualcomm/Hexagon_SDK/3.5.1/
+
+PATH=$SDK/tools/HEXAGON_Tools/8.3.07/Tools/bin:$PATH
+PATH=$SDK/tools/qaic/Ubuntu16:$PATH
+G0=$SDK/tools/HEXAGON_Tools/8.3.07/Tools/target/hexagon/lib/v60/G0
 
 mkdir -p out
 qaic -mdll -oout/ calculator.idl
 
 hexagon-clang -mv60 -fpic -shared \
-	-o out/libcalculator_skel.so \
-	-Iout -Iinclude \
+  -o out/libcalculator_skel.so \
+  -Iout -Iinclude \
   out/calculator_skel.c calculator_imp.c
 
